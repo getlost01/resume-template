9400cbc02d0b83f919a95b85dad69769d5ddea75
Aagam Jain <aagamjain573@gmail.com>
2022-07-01 15:40:55 +0530

    Add files via upload

--- scipt.js
+++ scipt.js
@@ -8,138 +8,150 @@ toggle.addEventListener('input',()=>{
     body.classList.add("dark");
 })
 
-var Exp = 1,Pro = 1,Cer = 1, Hon = 1,Info = 1;
+var genId = 0;
+var genArray = [];
 
 document.querySelector("#addExp").addEventListener('click',()=>{
     document.querySelector("#expSec").innerHTML +=`
-    <div class="row">
-    <h6>Experience ${++Exp} </h6>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="titleExp1"> Firm Name </label>
-        <input   type="text" placeholder="xyz firm" name="titleExp1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="designationExp1"> Designation </label>
-        <input   type="text" placeholder="SDE intern" name="designationExp1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="timeExp1"> Time Span </label>
-        <input   type="text" placeholder="22 june 2022 - present" name="timeExp1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="techExp1"> Technology experience </label>
-        <input   type="text" placeholder="nodeJS , reactjs , mongoose , JavaScript" name="techExp1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="descrpExp1"> Description </label>
-        <input   type="text" placeholder="My work is to improve efficiency of backend server and implement new features." name="descrpExp1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="linkExp1"> Any link to share </label>
-        <input   type="url" placeholder="eg: https://My-Work-Is-This.com" name="linkExp1" >
-    </div>
+            <div class="row" id="expSec${genId}">
+                <div class="titleBox"><h6>Experience Detail<i class="fa-solid fa-trash-can" id="delExp${genId}"></i></h6></div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="titleExp"> Firm Name </label>
+                    <input   type="text" required placeholder="xyz firm" name="titleExp" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="designationExp"> Designation </label>
+                    <input   type="text" required placeholder="SDE intern" name="designationExp" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="timeExp"> Time Span </label>
+                    <input   type="text" required placeholder="22 june 2022 - present" name="timeExp" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="techExp"> Technology experience </label>
+                    <input   type="text" required placeholder="nodeJS , reactjs , mongoose , JavaScript" name="techExp" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="descrpExp"> Description </label>
+                    <input   type="text" required placeholder="My work is to improve efficiency of backend server and implement new features." name="descrpExp" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="linkExp"> Any link to share </label>
+                    <input   type="url" placeholder="eg: https://My-Work-Is-This.com" name="linkExp" >
+                </div>
+             </div>
+
     `;
+    genArray.push({"ff":"delExp","ss":"expSec"});
+    ++genId;
+    render();
 })
 
 document.querySelector("#addPro").addEventListener('click',()=>{
     document.querySelector("#projectSec").innerHTML +=`
-    <div class="row">
-    <h6>Project ${++Pro}</h6>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="titlePro1"> Project Name </label>
-        <input   type="text" placeholder="CP Calender" name="titlePro1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="timePro1"> Time Span </label>
-        <input   type="text" placeholder="30 jan 2022 - 24 feb 2022" name="timePro1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="techPro1"> Technology Used </label>
-        <input   type="text" placeholder="nodeJS , reactjs , mongoose , JavaScript" name="techPro1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="descrpPro1"> Description </label>
-        <input   type="text" placeholder="show details related upcoming CP Contests" name="descrpPro1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="linkPro1"> Any link to share </label>
-        <input   type="url" placeholder="eg: https://My-Is-My-Project.com" name="linkPro1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="gitPro1"> Github Repo </label>
-        <input   type="url" placeholder="eg: https://github.com/getlost01/cp-contest-calender" name="gitPro1" >
-    </div>
+        <div class="row" id="projectSec${genId}">
+                <div class="titleBox"><h6>Project <i class="fa-solid fa-trash-can" id="delPro${genId}"></i></h6></div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="titlePro"> Project Name </label>
+                    <input   type="text" required placeholder="CP Calender" name="titlePro" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="timePro"> Time Span </label>
+                    <input   type="text" required placeholder="30 jan 2022 - 24 feb 2022" name="timePro" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="techPro"> Technology Used </label>
+                    <input   type="text" required placeholder="nodeJS , reactjs , mongoose , JavaScript" name="techPro" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="descrpPro"> Description </label>
+                    <input   type="text" required placeholder="show details related upcoming CP Contests" name="descrpPro" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="linkPro"> Any link to share </label>
+                    <input   type="url" placeholder="eg: https://My-Is-My-Project.com" name="linkPro" >
+                </div>
+                <div class="col-12 inputStyle col-md-6">
+                    <label for="gitPro"> Github Repo </label>
+                    <input   type="url" placeholder="eg: https://github.com/getlost01/cp-contest-calender" name="gitPro" >
+                </div>
+        </div>
     `;
+
+    genArray.push({"ff":"delPro","ss":"projectSec"});
+    ++genId;
+    render();
 })
 
 document.querySelector("#addCer").addEventListener('click',()=>{
     document.querySelector("#certifySec").innerHTML +=`
-    <div class="row">
-    <h6>Certificate ${++Cer}</h6>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="titleCer1"> Certificate Name </label>
-        <input   type="text" placeholder="Front-End Web UI Frameworks and Tools" name="titleCer1" >
+    <div class="row" id="certifySec${genId}">
+        <div class="titleBox"><h6>Certificate <i class="fa-solid fa-trash-can" id="delCer${genId}"></i></h6></div>
+        <div class="col-12 inputStyle col-md-6">
+            <label for="titleCer"> Certificate Name </label>
+            <input   type="text" required placeholder="Front-End Web UI Frameworks and Tools" name="titleCer" >
+        </div>
+        <div class="col-12 inputStyle col-md-6">
+            <label for="issCer"> Issued By </label>
+            <input   type="text" required placeholder="Coursera" name="issCer">
+        </div>
+        <div class="col-12 inputStyle col-md-6">
+            <label for="descrpCer"> Description </label>
+            <input   type="text" placeholder="For learning front end development." name="descrpCer" >
+        </div>
+        <div class="col-12 inputStyle col-md-6">
+            <label for="linkCer"> Certificate Link </label>
+            <input   type="url" required placeholder="eg: https://My-Is-My-Certificate.com" name="linkCer" >
+        </div>
     </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="issCer1"> Issued By </label>
-        <input   type="text" placeholder="Coursera" name="issCer1">
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="descrpCer1"> Description </label>
-        <input   type="text" placeholder="For learning front end development." name="descrpCer1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="linkCer1"> Any link to share </label>
-        <input   type="url" placeholder="eg: https://My-Is-My-Certificate.com" name="linkCer1" >
-    </div>
-
     `;
+
+    genArray.push({"ff":"delCer","ss":"certifySec"});
+    ++genId;
+    render();
 })
 
 document.querySelector("#addHon").addEventListener('click',()=>{
     document.querySelector("#honorSec").innerHTML +=`
-    <div class="row" >
-    <h6>Achievement ${++Hon}</h6>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="titleHon1"> Achievements name </label>
-        <input   type="text" placeholder="Get ranked 28 in conest by codechef." name="titleHon1">
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="recHon1"> Recognised by </label>
-        <input   type="text" placeholder="Codechef" name="recHon1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="descrpHon1"> Description </label>
-        <input   type="text" placeholder="In xyz contest I got 28 rank out of 1600+ students." name="descrpHon1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="linkHon1"> Any link to share </label>
-        <input   type="url" placeholder="eg: https://My-Is-My-Honor.com" name="linkHon1" >
-    </div>
+        <div class="row" id="honorSec${genId}" >
+
+            <div class="titleBox"><h6>Achievement <i class="fa-solid fa-trash-can" id="delHon${genId}"></i></h6></div>
+            <div class="col-12 inputStyle col-md-6">
+                <label for="descrpHon"> Description </label>
+                <input   type="text" required placeholder="In xyz contest I got 28 rank out of 1600+ students." name="descrpHon" >
+            </div>
+            <div class="col-12 inputStyle col-md-6">
+                <label for="linkHon"> Any link to share </label>
+                <input   type="url" placeholder="eg: https://My-Is-My-Honor.com" name="linkHon" >
+            </div>
+
+        </div>
     `;
+
+    genArray.push({"ff":"delHon","ss":"honorSec"});
+    ++genId;
+    render();
 })
 
 document.querySelector("#addInfo").addEventListener('click',()=>{
     document.querySelector("#infoSec").innerHTML +=`
-    <div class="row" >
-    <h6>Info ${++Info}</h6>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="titleInfo1">  Info name </label>
-        <input   type="text" placeholder="I also like digital art." name="titleInfo1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="recInfo1"> Related to </label>
-        <input   type="text" placeholder="Content Creation" name="recInfo1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="descrpInfo1"> Description </label>
-        <input   type="text" placeholder="I have create 50+ digital art / posters for social media." name="descrpInfo1" >
-    </div>
-    <div class="col-12 inputStyle col-md-6">
-        <label for="linkInfo1"> Any link to share </label>
-        <input   type="url" placeholder="eg: https://My-Is-My-Art.com" name="linkInfo1" >
+    <div class="row" id="infoRow${genId}">
+        <div class="titleBox"><h6>Info<i class="fa-solid fa-trash-can" id="delInfo${genId}"></i></h6></div>
+        
+        <div class="col-12 inputStyle col-md-6">
+            <label for="descrpInfo"> Description </label>
+            <input   type="text" required placeholder="I have create 50+ digital art / posters for social media." name="descrpInfo" >
+        </div>
+        <div class="col-12 inputStyle col-md-6">
+            <label for="linkInfo"> Any link to share </label>
+            <input   type="url" placeholder="eg: https://My-Is-My-Art.com" name="linkInfo" >
+        </div>
     </div>
     `;
+
+    genArray.push({"ff":"delInfo","ss":"infoRow"});
+    ++genId;
+    render();
 })
 
 document.querySelector("#okcheck").addEventListener('click',()=>{
@@ -148,3 +160,15 @@ document.querySelector("#okcheck").addEventListener('click',()=>{
     else
     document.querySelector("#submitBTN").disabled = true;
 })
+
+
+function render(){
+    genArray.forEach((ele,i) => {
+
+     if(ele.ff != "empty") 
+     document.querySelector(`#${ele.ff}${i}`).addEventListener('click',()=>{
+        document.querySelector(`#${ele.ss}${i}`).innerHTML = "";
+        ele.ff="empty";
+     })
+    });
+}
